b290be6ed0c2cd56b94963dfde56ddae43c784f8
JeremiasEssert <j.essert@sap.com>
2023-07-04 11:00:31 +0200

    Update Record.java

--- src/A_28_Java8/Record.java
+++ src/A_28_Java8/Record.java
@@ -1,6 +1,34 @@
 package A_28_Java8;
 
-public class Record {
+public record Record(
+        int trikotnummer,
+        String name,
+        String position,
+        String birthday,
+        String club,
+        int games,
+        int goals
+) {
 
+    static int comparePlayerByNumber(Record p1, Record p2) {
+        return Integer.compare(p2.trikotnummer, p1.trikotnummer);
+    }
+
+    static int comparePlayerByName(Record p1, Record p2) {
+        return p1.name.compareTo(p2.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "trikotnummer=" + trikotnummer +
+                ", name='" + name +
+                ", position='" + position +
+                ", birthday='" + birthday +
+                ", club='" + club +
+                ", games=" + games +
+                ", goals=" + goals +
+                '}';
+    }
 
 }
