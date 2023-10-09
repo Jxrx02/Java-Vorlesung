0a1e7bfcd7cd741a1243a96851635209bb5b211e
JeremiasEssert <j.essert@sap.com>
2023-10-09 15:33:47 +0200

    joa

--- src/AA_Klausuren/Probeklausr_SnatChat/SnatChat.java
+++ src/AA_Klausuren/Probeklausr_SnatChat/SnatChat.java
@@ -1,10 +1,14 @@
+package AA_Klausuren.Probeklausr_SnatChat;
+
 public class SnatChat {
-	
-	public static void main(String[] args) {
-		SnatChatRoom room = new SnatChatRoom("GansGeheim");
-		
-		room.register( new SnatChatWindow(room, new Account("Bob") ) );
-		room.register( new SnatChatWindow(room, new Account("Alice") ) );
-	}
-	
+
+  public static void main(String[] args) {
+    SnatChatRoom room = new SnatChatRoom("GansGeheim");
+
+    room.register(new SnatChatWindow(room, new Account("Bob")));
+    room.register(new SnatChatWindow(room, new Account("Alice")));
+
+
+  }
+
 }
